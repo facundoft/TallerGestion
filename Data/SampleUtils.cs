cfb1acc3f9ef129b885ce097e4a1c8727f9b7e58
samuel.mindler <samuel.mindler@estudiantes.utec.edu.uy>
2024-11-03 21:07:40 -0300

    3 graficas pero se rompe todo al actualizar, da error de JSInterop anda a saber que ***** es

---
+++ Data/SampleUtils.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Text;
+
+namespace TallerGestion.Data
+{
+    public static class SampleUtils
+    {
+       
+    }
+}
